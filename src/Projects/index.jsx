32c15e5174c5ba36dae8e6208b0f78d09a48eee5
DUREISSEIX Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2019-05-01 19:51:39 +0200

    add wip to projects section

--- src/Projects/index.jsx
+++ src/Projects/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import GoBackButton from "../Components/GoBackButton";
+import { Typography } from "@material-ui/core";
 import { withMenu } from "../EnrichmentMenu";
 import { withTranslation } from "react-i18next";
-import GoBackButton from "../Components/GoBackButton";
 
 class Projects extends React.Component {
 	render() {
+		const { t } = this.props;
 		return (
 			<React.Fragment>
 				<GoBackButton />
+				<Typography variant="body2">{t("WIP")}</Typography>
 			</React.Fragment>
 		);
 	}
