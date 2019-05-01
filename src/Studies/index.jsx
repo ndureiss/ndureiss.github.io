15aa6c64b087734f08b1e6b2ea6c37d4d491b12d
DUREISSEIX Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2019-05-01 20:41:36 +0200

    best handle of panel in study tab

--- src/Studies/index.jsx
+++ src/Studies/index.jsx
@@ -17,7 +17,7 @@ class Studies extends React.Component {
 
 	handleChange = panel => (event, expanded) => {
 		this.setState({
-			expanded: expanded ? panel : false,
+			expanded: panel,
 		});
 	};
 
