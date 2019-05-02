f060662095a5fac39074c8a9e3d34b8431faa445
DUREISSEIX Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2019-05-02 18:02:25 +0200

    handle non matched url

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "../Home";
 import TemporaryHome from "../TemporaryHome";
 import Studies from "../Studies";
 import WorkExperiences from "../WorkExperiences";
 import Projects from "../Projects";
 
+class NoMatch extends React.Component {
+	render() {
+		return <Redirect to="/" />;
+	}
+}
+
 class Routes extends React.Component {
 	render() {
 		return (
-			<div>
+			<Switch>
 				<Route exact path="/" component={TemporaryHome} />
 				<Route exact path="/home" component={Home} />
 				<Route exact path="/studies" component={Studies} />
 				<Route exact path="/work" component={WorkExperiences} />
 				<Route exact path="/projects" component={Projects} />
-			</div>
+				<Route component={() => <Redirect to="/" />} />
+			</Switch>
 		);
 	}
 }
